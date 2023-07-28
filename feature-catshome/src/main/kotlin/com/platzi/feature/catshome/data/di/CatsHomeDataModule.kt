c37e1e681d2c9180d641161790051f268bf36aee
Kperez <kevin.edgard.perez@gmail.com>
2023-07-27 23:35:51 -0500

    deleting unused classess

--- feature-catshome/src/main/kotlin/com/platzi/feature/catshome/data/di/CatsHomeDataModule.kt
+++ feature-catshome/src/main/kotlin/com/platzi/feature/catshome/data/di/CatsHomeDataModule.kt
@@ -3,15 +3,11 @@ package com.platzi.feature.catshome.data.di
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import com.platzi.feature.catshome.data.interceptor.AuthInterceptor
-import com.platzi.feature.catshome.data.moshi.DateMoshiAdapter
-import com.platzi.feature.catshome.data.moshi.DefaultIfNullFactory
 import com.platzi.feature.catshome.data.remote.CatRemoteMediator
 import com.platzi.feature.catshome.data.remote.CatsApi
 import com.platzi.randomcats.core.database.di.CatDatabase
 import com.platzi.randomcats.core.database.model.CatEntity
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,7 +15,6 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
